339f4677359ba8cb23fdedff4732ac78b7202955
unknown <jimmyjam100@aol.com>
2019-10-01 17:22:25 -0400

    started stuff

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 class ClassCalculator {
     constructor(data) {
-        this.data = data;
+        this.categories = data.categories.map(c => Object.assign({}, c, { grades: data.grades.filter(a => a.category === c.name) }))
+        this.grades = data.grades;
+    }
+    getCurrentGrade(){
+        let total = 0;
+        this.categories.forEach(element => {
+            total += getCatGrade(element);
+        });
+        return total;
+    }
+    getCatGrade(cat){
+        if (cat.droppedGrades){
+            cat.grades.sort(function(a, b){
+                if (!a.pointsEarned && !b.pointsEarned){
+                    return 0;
+                }
+                else if (!a.pointsEarned){
+                    return 1;
+                }
+                else if (!b.pointsEarned){
+                    return -1;
+                }
+                if (a.maxPoints - a.pointsEarned > b.maxPoints - b.pointsEarned){
+                    return -1;
+                }
+                else if(a.maxPoints - a.pointsEarned < b.maxPoints - b.pointsEarned){
+                    return 1;
+                }
+                return 0;
+            })
+            cat.grades.splice(0,droppedGrades);
+        }
+        if (cat.buildUp == true){
+            
+        }
     }
     
     getA() {
